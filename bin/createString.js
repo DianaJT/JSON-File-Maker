58cda8748c90635964e6df9056a1d5084d764f59
DianaJT <56441001+DianaJT@users.noreply.github.com>
2019-10-31 00:58:20 +0100

    Before concept change

---
+++ bin/createString.js
@@ -0,0 +1,99 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var htmlparser = __importStar(require("htmlparser2"));
+var fs_1 = __importDefault(require("fs"));
+var soupselect_1 = __importDefault(require("soupselect"));
+var saveHTML_1 = __importDefault(require("./saveHTML"));
+function createString() {
+    return __awaiter(this, void 0, void 0, function () {
+        var dom, headers, outputString, j, err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, saveHTML_1.default('https://github.com/sindresorhus/awesome-nodejs', 'temp/awesome-nodejs.html')];
+                case 1:
+                    _a.sent();
+                    dom = htmlparser.parseDOM(fs_1.default.readFileSync('temp/awesome-nodejs.html').toString());
+                    headers = soupselect_1.default.select(dom, 'h3');
+                    outputString = '';
+                    outputString = outputString.concat('{\n  github: {\n    package: "', headers[3].children[1].data, '",\n    projects: [');
+                    outputString = outputString.concat('{\n      name: "', headers[3].next.next.children[1].children[0].children[0].data);
+                    outputString = outputString.concat('",\n      url: "', headers[3].next.next.children[1].children[0].attribs.href);
+                    outputString = outputString.concat('",\n      description: "', headers[3].next.next.children[1].children[1].data, '"\n    }');
+                    for (j = 3; j < headers[3].next.next.children.length; j += 2) {
+                        outputString = outputString.concat(', {\n      name: "', headers[3].next.next.children[j].children[0].children[0].data);
+                        outputString = outputString.concat('",\n      url: "', headers[3].next.next.children[j].children[0].attribs.href);
+                        outputString = outputString.concat('",\n      description: "', headers[3].next.next.children[j].children[1].data, '"\n    }');
+                    }
+                    outputString = outputString.concat(']\n  }');
+                    // for (let i = 4; i < headers.length - 12; i += 1) {
+                    //   outputString = outputString.concat(', {\n    package: "', headers[i].children[1].data, '",\n    projects: [');
+                    //   outputString = outputString.concat('{\n      name: "', headers[i].next.next.children[1].children[0].children[0].data);
+                    //   outputString = outputString.concat('",\n      url: "', headers[i].next.next.children[1].children[0].attribs.href);
+                    //   outputString = outputString.concat('",\n      description: "', headers[i].next.next.children[1].children[1].data, '"\n    }');
+                    //   for (let j = 3; j < headers[i].next.next.children.length; j += 2) {
+                    //     outputString = outputString.concat(', {\n      name: "', headers[i].next.next.children[j].children[0].children[0].data);
+                    //     outputString = outputString.concat('",\n      url: "', headers[i].next.next.children[j].children[0].attribs.href);
+                    //     outputString = outputString.concat('",\n      description: "', headers[i].next.next.children[j].children[1].data, '"\n    }');
+                    //   }
+                    //   outputString = outputString.concat(']\n  }');
+                    // }
+                    outputString = outputString.concat('\n}');
+                    return [2 /*return*/, outputString];
+                case 2:
+                    err_1 = _a.sent();
+                    console.error(err_1);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.default = createString;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import saveHTML from './saveHTML';
-import createJSON from './createJSON';
+import createString from './createString';
 
-saveHTML('https://github.com/sindresorhus/awesome-nodejs', 'temp/awesome-nodejs.html').then(() => createJSON());
+// saveHTML('https://github.com/sindresorhus/awesome-nodejs', 'temp/awesome-nodejs.html').then(() => createString());
 
-// async function main() {
-//   try {
-//     await saveHTML('https://github.com/sindresorhus/awesome-nodejs', 'temp/awesome-nodejs.html');
-//     createJSON();
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// main();
+async function main() {
+  try {
+    await saveHTML('https://github.com/sindresorhus/awesome-nodejs', 'temp/awesome-nodejs.html');
+    const ourString = await createString();
+    fs.writeFileSync('temp/awesome-nodejs.json', JSON.stringify(ourString));
+  } catch (err) {
+    console.error(err);
+  }
+}
+main();
